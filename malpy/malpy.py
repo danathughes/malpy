e33da0ca27ecd095ae2c0106905e3a6776fcc3c6
Dana Hughes <danathughes@gmail.com>
2019-12-17 16:12:26 -0500

    Initial commit

---
+++ malpy/malpy.py
@@ -0,0 +1,10 @@
+## cyrk.py               Dana Hughes           17-Dec-2019
+##
+## A set of classes to simpify the interface between Python and 
+## Malmo.
+##
+## Revision History:
+##    17-Dec-2019    Initial Version.
+
+
+class 
